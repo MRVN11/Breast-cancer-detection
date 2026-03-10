3abe411a38383d1ac749e70520e3a73953af5302
rb011964 <rb011964@reading.ac.uk>
2026-03-10 12:28:34 +0000

    need to up the accuracy

--- Main.py
+++ Main.py
@@ -3,18 +3,16 @@ import ssl
 
 import tensorflow as tf
 from tensorflow.keras.applications import DenseNet121
-from tensorflow.keras.layers import  Dense, Dropout, Flatten
+from tensorflow.keras.layers import  Dense, Dropout, Flatten, GlobalAveragePooling2D
 from tensorflow.keras import Sequential
+
+from data_operations.data_preprocessing import dataset_stratified_split, import_MIAS_dataset
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-import cv2
 import numpy as np
-from imutils import paths
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-from sklearn.utils import shuffle, class_weight
+from sklearn.utils import  class_weight
 
 
 def main() -> None:
@@ -31,7 +29,7 @@ def main() -> None:
     class_weights = caculate_weights(y_train, l_e)
 
 # training the model
-    history = model.fit(X_train, y_train, epochs=10, batch_size=16, validation_data=(X_val, y_val), class_weight=class_weights)
+    history = model.fit(X_train, y_train, epochs=10, batch_size=8, validation_data=(X_val, y_val), class_weight=class_weights)
 
     for layer in model.layers[0].layers[-30:]:
         layer.trainable = True
@@ -41,7 +39,7 @@ def main() -> None:
         metrics=["accuracy"]
     )
 
-    model.fit(X_train, y_train, epochs=10, batch_size=16)
+    model.fit(X_train, y_train, epochs=30, batch_size=8)
 
 
     test_loss, test_acc = model.evaluate(X_test, y_test)
@@ -62,7 +60,8 @@ def create_desnet121(num_classes: int):
 
     model = Sequential([
         base_model,
-        Flatten(),
+        # Flatten(),
+        GlobalAveragePooling2D(),
         Dense(256, activation="relu"),
         Dropout(0.5),
         Dense(num_classes, activation="softmax" if num_classes > 2 else "sigmoid")
@@ -80,48 +79,15 @@ def create_desnet121(num_classes: int):
 def caculate_weights(y_train, label_encoder):
     if label_encoder.classes_.size != 2:
         y_train = label_encoder.inverse_transform(np.argmax (y_train, axis=1))
-    weights = class_weight.compute_class_weight('balanced', np.unique(y_train), y_train)
+
+    weights = class_weight.compute_class_weight('balanced',
+                                                classes =  np.unique(y_train),
+                                                y = y_train)
 
     class_weights = dict(enumerate(weights))
     return class_weights
 
 
-def dataset_stratified_split(split: float, dataset: np.ndarray, labels: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
-    """Split dataset into training and testing sets with stratification."""
-    train_x, test_x, train_y, test_y = train_test_split(dataset, labels, test_size=split, stratify=labels, random_state=42, shuffle=True)
-    return train_x, test_x, train_y, test_y
-
-def import_MIAS_dataset(data_dir: str, label_encoder) -> (np.ndarray, np.ndarray):
-    images = list()
-    labels = list()
-
-    for image_path in list(paths.list_images(data_dir)):
-        images.append(preprocess_images(image_path))
-        labels.append(image_path.split(os.path.sep)[-2])
-    images = np.array(images, dtype="float32")
-    labels = np.array(labels)
-
-    labels = encode_labels(labels, label_encoder)
-
-    images, labels = shuffle(images, labels, random_state=42)
-    return images, labels
-
-def preprocess_images(image_path: str) -> np.ndarray:
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    gray = clahe.apply(gray)
-    image = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (224, 224))
-    image = image.astype("float32") / 255.0
-    return image
-
-def encode_labels(labels_list: np.ndarray, label_encoder) -> np.ndarray:
-    labels = label_encoder.fit_transform(labels_list)
-    if label_encoder.classes_.size == 2:
-        return labels
-    else:
-        return to_categorical(labels)
 
 if __name__ == "__main__":
     main()
---
+++ data_operations/data_preprocessing.py
@@ -0,0 +1,51 @@
+import os
+
+import cv2
+import numpy as np
+from imutils import paths
+from keras.src.applications.densenet import preprocess_input
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+def preprocess_images(image_path: str) -> np.ndarray:
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    gray = clahe.apply(gray)
+    image = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (224, 224))
+    image = preprocess_input(image)
+    return image
+
+def encode_labels(labels_list: np.ndarray, label_encoder) -> np.ndarray:
+    labels = label_encoder.fit_transform(labels_list)
+    if label_encoder.classes_.size == 2:
+        return labels
+    else:
+        return to_categorical(labels)
+
+def dataset_stratified_split(split: float, dataset: np.ndarray, labels: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    """Split dataset into training and testing sets with stratification."""
+    train_x, test_x, train_y, test_y = train_test_split(dataset,
+                                                        labels,
+                                                        test_size=split,
+                                                        stratify=labels,
+                                                        random_state=42,
+                                                        shuffle=True)
+    return train_x, test_x, train_y, test_y
+
+def import_MIAS_dataset(data_dir: str, label_encoder) -> (np.ndarray, np.ndarray):
+    images = list()
+    labels = list()
+
+    for image_path in list(paths.list_images(data_dir)):
+        images.append(preprocess_images(image_path))
+        labels.append(image_path.split(os.path.sep)[-2])
+    images = np.array(images, dtype="float32")
+    labels = np.array(labels)
+
+    labels = encode_labels(labels, label_encoder)
+
+    images, labels = shuffle(images, labels, random_state=42)
+    return images, labels
